3829b74df13d9aea2881e1eec0558051dfe8d21f
Oziel Pacheco <ozielpacheco.m@gmail.com>
2022-02-19 11:22:54 -0600

    avances

--- src/app/models/pokemonDetails.ts
+++ src/app/models/pokemonDetails.ts
@@ -1,17 +1,41 @@
 export class PokemonDetail {
-    id: number;
-    order: number;
-    name: string;
-    height: number;
+    id: number = 0;
+    order: number = 0;
+    name: string = '';
+    height: number = 0;
     abilities: Ability[];
     spices: Species;
     types: Type[];
-    weight: number;
+    weight: number = 0;
     sprites: Sprite;
     stats: Stat[];
 
     constructor() {
         this.abilities  = [];
         this.types = [];
+    }
+}
+class Ability{
+    ability:{
+        name :string ;
+    }
+    constructor(){}
+}
+class Species {
+    url:string;
+}
+class Type{
+    slot:number;
+    type: {
+        name:string;
+    }
+}
+class Sprite{
+    front_default:string;
+}
+class Stat{
+    base_stat :number
+    stat: {
+        name: string
     }
 }
