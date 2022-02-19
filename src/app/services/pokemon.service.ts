81923bdd22365eeed4828e4839b6a472bfaf7505
Oziel Pacheco <ozielpacheco.m@gmail.com>
2022-02-19 11:39:27 -0600

    Se termina servicio

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,12 @@
 //parte fundamental, púes este archivo se comunica con nuestro back
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from "rxjs";
+import { Observable, map } from "rxjs";
+
+// modelos
+import { PokemonDetail } from "../models/pokemonDetails";
+import { PokemonList } from "../models/pokemonList";
+// servicios
 import { global } from "./global.service";
 
 @Injectable()
@@ -12,10 +17,22 @@ export class PokemonService{
     constructor(
         public _http: HttpClient
     ){
+
         this.url = global.url;
     }
     getResourcePokemon(namePokemon:any):Observable<any>{
         let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
         return this._http.get(this.url+'pokemon/'+namePokemon, {headers:headers} );
+    }
+
+    getPokemonList(offset: number, limit: number = 20):Observable<PokemonList[]>{
+        return this._http.get<PokemonList[]>(this.url + 'pokemon?offset=' + offset + '&limit=' + limit)
+        .pipe(
+            map((x: any) => x.result)
+        );
+    }
+
+    getPokemonDetail(pokemon: number | string):Observable<PokemonDetail>{
+        return this._http.get<PokemonDetail>(this.url + 'pokemon/' + pokemon);
     }
 }
